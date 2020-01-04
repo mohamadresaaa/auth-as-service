cf1245b263d7ad199631447a378b4e08fc5cdb8b
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-01-05 00:42:18 +0330

    install jest

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  presets: ["@babel/preset-env"],
+  presets: ["@babel/preset-env", {
+    targets: {
+      node: "current"
+    }
+  }],
   plugins: [
     ["@babel/plugin-transform-runtime",
       {
