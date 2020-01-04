ed8cca06e194610ffb952ae4bf3d5fe2a25c0237
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-01-05 00:59:50 +0330

    fix testing with jest

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,5 @@
 module.exports = {
-  presets: ["@babel/preset-env", {
-    targets: {
-      node: "current"
-    }
-  }],
+  presets: ["@babel/preset-env"],
   plugins: [
     ["@babel/plugin-transform-runtime",
       {
