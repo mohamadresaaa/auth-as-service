7898c820e3c77982c6fd961afc2134614a21a6e6
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-01-13 22:17:12 +0330

    register validation schema

--- src/utilities/validatorSchema.js
+++ src/utilities/validatorSchema.js
@@ -1,1 +1,7 @@
 import joi from "@hapi/joi"
+
+export const register = joi.object().keys({
+  email: joi.string().email({ minDomainSegments: 2 }).required(),
+  password: joi.string().min(8).required(),
+  username: joi.string().alphanum().required()
+})
