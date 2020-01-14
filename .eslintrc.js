2d7a415a2c94cb1374e11e558b6c8a50ca51f049
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-01-14 20:51:23 +0330

    set config variable in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,8 @@ module.exports = {
   ],
   globals: {
     Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
+    SharedArrayBuffer: "readonly",
+    config: "readonly"
   },
   parserOptions: {
     parser: "babel-eslint"
