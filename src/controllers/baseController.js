30a7ab986d1f19657f24c4de4b963cc9e6608733
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-01-14 13:16:54 +0330

    set models in baseController

--- src/controllers/baseController.js
+++ src/controllers/baseController.js
@@ -1,8 +1,12 @@
 import autoBind from "auto-bind"
+import models from "../models"
 
 export default class BaseController {
   constructor () {
     // binding methods for using on child classes
     autoBind(this)
+
+    // set models in symbol key
+    this[Symbol.for("models")] = models
   }
 }
