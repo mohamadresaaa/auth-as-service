1d5722c75e353154da777f5c1d5a41aceeee80fe
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-01-15 17:49:22 +0330

    set validator in routes

--- src/routes/v1/auth.js
+++ src/routes/v1/auth.js
@@ -1,12 +1,15 @@
+import { login, passwordRecovery, register, resetPassword } from "../../utilities/validatorSchema"
 import { v1 } from "../../controllers"
+import { validator } from "../../middleware/validator"
+
 const router = require("express").Router()
 
 // Define routes
-router.post("/register", v1.auth.registration)
-router.post("/login", v1.auth.login)
+router.post("/register", validator(register), v1.auth.registration)
+router.post("/login", validator(login), v1.auth.login)
 router.post("/twoFactorAuth", (req, res) => res.send("twoFactorAuth"))
-router.post("/recoveryPassword", v1.auth.forgotPassword)
-router.post("/resetPassword", v1.auth.resetPassword)
+router.post("/recoveryPassword", validator(passwordRecovery), v1.auth.forgotPassword)
+router.post("/resetPassword", validator(resetPassword), v1.auth.resetPassword)
 
 // Exports router
 module.exports = router
