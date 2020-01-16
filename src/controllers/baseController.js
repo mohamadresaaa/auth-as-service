1c7a27ec81e3a9850b4f673afc23b3ba0d9b2108
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-01-16 20:43:22 +0330

    add infoMessage and errorMessage

--- src/controllers/baseController.js
+++ src/controllers/baseController.js
@@ -1,6 +1,7 @@
 const autoBind = require("auto-bind")
 const models = require("../models")
 const services = require("../services")
+const { ErrorMessage } = require("../lib/messages")
 
 module.exports = class BaseController {
   constructor () {
@@ -13,4 +14,22 @@ module.exports = class BaseController {
     // Set services
     this[Symbol.for("services")] = services
   }
+
+  /** Show public info message
+   * @param response express 
+   * @param data
+   * @return response
+   */
+  infoMessage(res, data = { name, message, status, properties }) {
+    res.status(status).json(data)
+  }
+
+  /** Show error message
+   * @param data
+   * @return errorMessage
+   */
+  errorMessage(data = { name, message, status, properties }) {
+    throw new ErrorMessage({ ...data })
+  }
+
 }
