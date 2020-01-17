5eea74fd99a6b76d4cc91013cc44fbb438d3ead5
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-01-17 13:58:37 +0330

    remove errorMessage method

--- src/controllers/baseController.js
+++ src/controllers/baseController.js
@@ -1,7 +1,6 @@
 const autoBind = require("auto-bind")
 const models = require("../models")
 const services = require("../services")
-const { ErrorMessage } = require("../lib/messages")
 
 module.exports = class BaseController {
   constructor () {
@@ -25,18 +24,6 @@ module.exports = class BaseController {
    */
   infoMessage (res, data) {
     res.status(data.status).json(data)
-  }
-
-  /** Show error message
-   * @param {object} data
-   * @param {string} data.name
-   * @param {string} data.message
-   * @param {number} data.status
-   * @param {any} data.properties
-   * @return errorMessage
-   */
-  errorMessage (data) {
-    throw new ErrorMessage({ ...data })
   }
 
 }
