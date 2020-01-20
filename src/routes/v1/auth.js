055d426747ebc336f181eadfe1964a33d16eecc5
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-01-20 13:51:00 +0330

    add verifyCode route to auth router

--- src/routes/v1/auth.js
+++ src/routes/v1/auth.js
@@ -1,5 +1,5 @@
 const {
-  login, passwordRecovery, register, resetPassword
+  login, passwordRecovery, register, resetPassword, verifyCode
 } = require("../../utilities/validatorSchema")
 const { v1 } = require("../../controllers")
 const validator = require("../../middleware/validator")
@@ -13,6 +13,7 @@ router.get("/twoFactorAuth", (req, res) => {
   res.json(req.device)
 })
 router.post("/passwordRecovery", validator(passwordRecovery), v1.auth.forgotPassword)
+router.post("/verifyCode", validator(verifyCode), v1.auth.verifyCode)
 router.post("/resetPassword", validator(resetPassword), v1.auth.resetPassword)
 
 // Exports router
