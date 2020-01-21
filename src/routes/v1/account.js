6a891d99b6a327d223d4249b37169dcadb25793d
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-01-22 00:38:25 +0330

    add reactivation route

--- src/routes/v1/account.js
+++ src/routes/v1/account.js
@@ -3,8 +3,17 @@ const router = require("express").Router()
 // Controllers
 const { v1 } = require("../../controllers")
 
+// Middleware
+const validator = require("../../middleware/validator")
+
+// Validation schemas
+const {
+  reactivation
+} = require("../../utilities/validatorSchema")
+
 // Define routes
 router.get("/activation/:code", v1.account.activation)
+router.post("/reactivation", validator(reactivation), v1.account.reactivation)
 
 // Exports router
 module.exports = router
