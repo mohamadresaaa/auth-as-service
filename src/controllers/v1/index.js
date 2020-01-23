819a58b47bbcb50e9beb30aa4085a3122fef89f1
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-01-23 12:45:46 +0330

    import controllers

--- src/controllers/v1/index.js
+++ src/controllers/v1/index.js
@@ -1,9 +1,14 @@
-const account = require("./accountController")
-const auth = require("./authController")
-const user = require("./userController")
+const fs = require("fs")
+const path = require("path")
 
-module.exports = {
-  account,
-  auth,
-  user
+const v1 = {}
+
+// Read controllers from directory
+const controllers = fs.readdirSync(path.resolve(__dirname)).filter((item) => !item.match("index.js"))
+
+// Import to v1
+for (const controller of controllers) {
+  v1[controller.replace("Controller.js", "")] = require(`${path.resolve(__dirname, controller)}`)
 }
+
+module.exports = v1
