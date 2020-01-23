a443be22cc4111b7df1cd9a380eb5d2703484331
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-01-24 00:12:51 +0330

    add sessions routes

--- src/routes/v1/session.js
+++ src/routes/v1/session.js
@@ -1,17 +1,13 @@
 const router = require("express").Router()
-const sessionRoutes = require("./session")
 
 // Controllers
 const { v1 } = require("../../controllers")
 
 /** @define routes */
 
-// Show user information
+// User account activation
 router.get("/",
-  v1.user.profile)
-
-router.use("/sessions",
-  sessionRoutes)
+  v1.session.list)
 
 // Exports router
 module.exports = router
