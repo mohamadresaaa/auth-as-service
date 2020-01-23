70ebc81d6f346d70f762b61295fa59eed8e107f0
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-01-23 15:13:14 +0330

    update importing services

--- src/services/v1/index.js
+++ src/services/v1/index.js
@@ -1,21 +1,21 @@
-const activation = require("./account/activation")
-const login = require("./auth/login")
-const logout = require("./auth/logout")
-const passwordRecovery = require("./auth/passwordRecovery")
-const profile = require("./user/profile")
-const reactivation = require("./account/reactivation")
-const register = require("./auth/register")
-const resetPassword = require("./auth/resetPassword")
-const verifyCode = require("./auth/verifyCode")
+const fs = require("fs")
+const path = require("path")
 
-module.exports = {
-  activation,
-  login,
-  logout,
-  passwordRecovery,
-  profile,
-  reactivation,
-  register,
-  resetPassword,
-  verifyCode
+const services = {}
+
+// Read and find directories
+const findDirectories = fs.readdirSync(path.resolve(__dirname)).filter((item) => !item.match("index.js"))
+
+// Convert directories to object
+for (const directory of findDirectories) {
+  services[directory] = {}
 }
+
+// Read files of directory
+Object.keys(services).map(key => {
+  for (const file of fs.readdirSync(path.resolve(__dirname, key))) {
+    services[key][file.replace(".js", "")] = require(path.resolve(__dirname, key, file))
+  }
+})
+
+module.exports = services
