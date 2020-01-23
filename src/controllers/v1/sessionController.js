0db46d5b44b1a944af776e13d272c54c1575f590
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-01-23 11:13:13 +0330

    add session controller

---
+++ src/controllers/v1/sessionController.js
@@ -0,0 +1,12 @@
+const baseController = require("../baseController")
+
+// eslint-disable-next-line new-parens
+module.exports = new class SessionController extends baseController {
+  async list (req, res, next) {
+
+  }
+
+  async remove (req, res, next) {
+
+  }
+}
