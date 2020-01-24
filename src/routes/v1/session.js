9e613024c89b5de851c965ceb1bd675cf3233c7a
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-01-24 12:12:05 +0330

    add delete session route

--- src/routes/v1/session.js
+++ src/routes/v1/session.js
@@ -5,9 +5,11 @@ const { v1 } = require("../../controllers")
 
 /** @define routes */
 
-// User account activation
+// List of sessions user
 router.get("/",
   v1.session.list)
+
+router.delete("/:id", v1.session.remove)
 
 // Exports router
 module.exports = router
