fb2e3cddde89bddfa705d19891e47d26bee56487
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-01-28 16:01:42 +0330

    rename controller

---
+++ src/controllers/v1/analyticsController.js
@@ -0,0 +1,8 @@
+const baseController = require("../baseController")
+
+// eslint-disable-next-line new-parens
+module.exports = new class analyticsController extends baseController {
+  async usersCondition (req, res, next) {
+
+  }
+}
--- src/controllers/v1/userAnalyticsController.js
+++
@@ -1,8 +0,0 @@
-const baseController = require("../baseController")
-
-// eslint-disable-next-line new-parens
-module.exports = new class UserAnalyticsController extends baseController {
-  async condition (req, res, next) {
-
-  }
-}
