0dc07f2a5e69f9d70e77feb4be529a1cb90d0702
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-01-28 18:43:19 +0330

    set users condition sevice in controller

--- src/controllers/v1/analyticsController.js
+++ src/controllers/v1/analyticsController.js
@@ -3,6 +3,10 @@ const baseController = require("../baseController")
 // eslint-disable-next-line new-parens
 module.exports = new class analyticsController extends baseController {
   async usersCondition (req, res, next) {
-
+    try {
+      await this[Symbol.for("services")].v1.analytics.usersCondition(this, res)
+    } catch (error) {
+      next(error)
+    }
   }
 }
