6957523b656224b48e50bc468eb73bd1411de31f
mohamadreza mosalli <mohamadresaaa@gmail.com>
2020-01-28 15:59:52 +0330

    add user analytics controller

---
+++ src/controllers/v1/userAnalyticsController.js
@@ -0,0 +1,8 @@
+const baseController = require("../baseController")
+
+// eslint-disable-next-line new-parens
+module.exports = new class UserAnalyticsController extends baseController {
+  async condition (req, res, next) {
+
+  }
+}
